d8af51983f6eac42ea12fd79b71c59c475d89102
himawanTIF <40656518+himawanTIF@users.noreply.github.com>
2022-07-24 19:38:27 +0700

    Update undian.sql
    Perbaikan tipe data formulir

--- db/undian.sql
+++ db/undian.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.0.1
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 24, 2022 at 11:41 AM
--- Server version: 10.1.32-MariaDB
--- PHP Version: 7.2.5
+-- Generation Time: Jul 24, 2022 at 02:37 PM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -30,8 +30,8 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `formulir` (
   `nama` varchar(50) NOT NULL,
-  `nik` int(15) NOT NULL,
-  `hp` int(12) NOT NULL
+  `nik` bigint(15) NOT NULL,
+  `hp` bigint(12) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -40,7 +40,9 @@ CREATE TABLE `formulir` (
 
 INSERT INTO `formulir` (`nama`, `nik`, `hp`) VALUES
 ('angga', 2147483647, 2147483647),
-('trisna', 2147483647, 2147483647);
+('trisna', 2147483647, 2147483647),
+('jhonny', 2147483647, 2147483647),
+('Thomas', 1695233355589645, 81223546945);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
